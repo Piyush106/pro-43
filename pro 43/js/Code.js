bcccea509b4cacaa8c21db861a550109d63c1ae6
Piyush21Solanki <77489530+Piyush21Solanki@users.noreply.github.com>
2021-07-30 14:52:17 +0530

    Add files via upload

---
+++ pro 43/js/Code.js
@@ -0,0 +1,30 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+const accessCode4 = "ANIMATION";
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 700,150)
+    fill("lightblue")
+    text("Hint: Performs a particular task !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("A T E D A S B A", 100,250)
+    fill("lightblue")
+    text("Hint: Stores all information !!", 100,270)
+
+    fill("white")
+    textSize(15)
+    text("N N A A M T N O I", 500,300)
+    fill("lightblue")
+    text("Hint: IT displays objects nicely !!", 500,320)
+}
